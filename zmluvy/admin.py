76f161f772f2db60621a78ce29bcb41ef29fd6a0
milossramek <msramek22@gmail.com>
2021-02-10 18:48:39 +0100

    model modifications, show all field in admin view

--- zmluvy/admin.py
+++ zmluvy/admin.py
@@ -3,5 +3,21 @@ from django.contrib import admin
 # Register your models here.
 from .models import Osoba, Zmluva
 
-admin.site.register(Osoba)
-admin.site.register(Zmluva)
+
+class OsobaAdmin(admin.ModelAdmin):
+    list_display = ('rs_uid', 'rs_login', 'email', 'titul_pred_menom', 'meno', 'priezvisko', 'titul_za_menom', 'posobisko', 'datum_pridania', 'datum_aktualizacie')
+    ordering = ('datum_aktualizacie',)
+    #search_fields = ('rs_login', 'priezvisko')
+    search_fields = ['rs_login']
+
+admin.site.register(Osoba, OsobaAdmin)
+
+class ZmluvaAdmin(admin.ModelAdmin):
+    list_display = ('zmluvna_strana', 'cislo_zmluvy', 'datum_pridania', 'datum_aktualizacie')
+    ordering = ('datum_aktualizacie',)
+    search_fields = ['cislo_zmluvy']
+
+admin.site.register(Zmluva, ZmluvaAdmin)
+
+#admin.site.register(Osoba)
+#admin.site.register(Zmluva)
--- zmluvy/models.py
+++ zmluvy/models.py
@@ -7,37 +7,36 @@ class Osoba(models.Model):
     rs_uid = models.IntegerField("Uid v RS")
     rs_login = models.CharField("Login v RS", max_length=100)
     email = models.CharField("Email", max_length=200)
-    titul_pred_menom = models.CharField("Titul pred menom", max_length=100, blank=True)
+    titul_pred_menom = models.CharField("Titul pred menom", max_length=100, blank=True) #optional
     meno = models.CharField("Meno", max_length=200)
     priezvisko = models.CharField("Priezvisko", max_length=200)
-    titul_za_menom = models.CharField("Titul za menom", max_length=100, blank=True)
-    posobisko = models.CharField("Pôsobisko", max_length=200, blank=True)
-    datum_pridania = models.DateTimeField('Dátum pridania')
-    datum_aktualizacie = models.DateTimeField('Dátum aktualizácie')
+    titul_za_menom = models.CharField("Titul za menom", max_length=100, blank=True)     #optional
+    posobisko = models.CharField("Pôsobisko", max_length=200, blank=True)               #optional
+    datum_pridania = models.DateTimeField('Dátum pridania', auto_now_add=True)
+    datum_aktualizacie = models.DateTimeField('Dátum aktualizácie', auto_now=True)
     #pub_date = models.DateTimeField('date published')
 
     def __str__(self):
         return self.rs_login
-    def nedavno_pridane(self):
-        return self.datum_aktualizacie >= timezone.now() - datetime.timedelta(days=1)
 
 #1057	TrnkaAlfred	atrnka@truni.sk	prof. RNDr.	Alfréd	Trnka	PhD.	1729
 #   oo = Osoba(rs_uid = 1057, rs_login = "TrnkaAlfred", email = "atrnka@truni.sk", titul_pred_menom = "RNDr.", meno = "Alfréd", priezvisko = "Trnka", titul_za_menom = "PhD.", posobisko = "Truni, Trenčín")
+#   oo.save()
+#   zz=Zmluva(zmluvna_strana=oo, cislo_zmluvy="1729")
+#   zz.save()
 
 #1062	AstalosBoris	boris.astalos@SNM.sk		Boris	Astaloš		40/2019
 #   oo = Osoba(rs_uid = 1062, rs_login = "AstalosBoris", email = "boris.astalos@SNM.sk", meno = "Boris", priezvisko = "Astaloš", posobisko = "Slovenské národné múzeum, Bratislava")
-#z=Zmluva(zmluvna_strana=oo, cislo_zmluvy="40/2019")
-
-    
+#   oo.save()
+#   zz=Zmluva(zmluvna_strana=oo, cislo_zmluvy="40/2019")
+#   zz.save()
 
 class Zmluva(models.Model):
     zmluvna_strana = models.ForeignKey(Osoba, on_delete=models.CASCADE)
     cislo_zmluvy = models.CharField("Číslo zmluvy", max_length=50)
-    datum_pridania = models.DateTimeField('Dátum pridania')
-    datum_aktualizacie = models.DateTimeField('Dátum aktualizácie')
+    datum_pridania = models.DateTimeField('Dátum pridania', auto_now_add=True)
+    datum_aktualizacie = models.DateTimeField('Dátum aktualizácie', auto_now=True)
 
     def __str__(self):
         return self.cislo_zmluvy
-    def nedavno_aktualizovane(self):
-        return self.datum_aktualizacie >= timezone.now() - datetime.timedelta(days=1)
 
