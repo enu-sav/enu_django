7fd95899cdbec3c8255bf0cca68320a10b8d2c77
milossramek <msramek22@gmail.com>
2021-02-10 18:57:51 +0100

    reorder list_display, so that the right one is highlighted

--- zmluvy/admin.py
+++ zmluvy/admin.py
@@ -5,7 +5,7 @@ from .models import Osoba, Zmluva
 
 
 class OsobaAdmin(admin.ModelAdmin):
-    list_display = ('rs_uid', 'rs_login', 'email', 'titul_pred_menom', 'meno', 'priezvisko', 'titul_za_menom', 'posobisko', 'datum_pridania', 'datum_aktualizacie')
+    list_display = ('rs_login', 'rs_uid', 'email', 'titul_pred_menom', 'meno', 'priezvisko', 'titul_za_menom', 'posobisko', 'datum_pridania', 'datum_aktualizacie')
     ordering = ('datum_aktualizacie',)
     #search_fields = ('rs_login', 'priezvisko')
     search_fields = ['rs_login']
@@ -20,14 +20,14 @@ class OsobaAdmin(admin.ModelAdmin):
 admin.site.register(Osoba, OsobaAdmin)
 
 class ZmluvaAdmin(admin.ModelAdmin):
-    list_display = ('zmluvna_strana', 'cislo_zmluvy', 'datum_pridania', 'datum_aktualizacie')
+    list_display = ('cislo_zmluvy', 'zmluvna_strana', 'datum_pridania', 'datum_aktualizacie')
     ordering = ('datum_aktualizacie',)
     search_fields = ['cislo_zmluvy']
 
     #obj is None during the object creation, but set to the object being edited during an edit
     def get_readonly_fields(self, request, obj=None):
         if obj:
-            return ["zmluvna_strana", "cislo_zmluvy"]
+            return ["cislo_zmluvy", "zmluvna_strana"]
         else:
             return []
 
