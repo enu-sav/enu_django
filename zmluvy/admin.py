7845a80ef82cfdc742cb41034a634a7774d963be
milossramek <msramek22@gmail.com>
2021-02-10 16:27:15 +0100

    added superuser, some fields set as optional

--- zmluvy/admin.py
+++ zmluvy/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Osoba, Zmluva
+
+admin.site.register(Osoba)
+admin.site.register(Zmluva)
--- zmluvy/models.py
+++ zmluvy/models.py
@@ -7,18 +7,19 @@ class Osoba(models.Model):
     rs_uid = models.IntegerField("Uid v RS")
     rs_login = models.CharField("Login v RS", max_length=100)
     email = models.CharField("Email", max_length=200)
-    titul_pred_menom = models.CharField("Titul pred menom", max_length=100)
+    titul_pred_menom = models.CharField("Titul pred menom", max_length=100, blank=True)
     meno = models.CharField("Meno", max_length=200)
     priezvisko = models.CharField("Priezvisko", max_length=200)
-    titul_za_menom = models.CharField("Titul za menom", max_length=100)
-    posobisko = models.CharField("Pôsobisko", max_length=200)
+    titul_za_menom = models.CharField("Titul za menom", max_length=100, blank=True)
+    posobisko = models.CharField("Pôsobisko", max_length=200, blank=True)
     datum_pridania = models.DateTimeField('Dátum pridania')
+    datum_aktualizacie = models.DateTimeField('Dátum aktualizácie')
     #pub_date = models.DateTimeField('date published')
 
     def __str__(self):
         return self.rs_login
     def nedavno_pridane(self):
-        return self.datum_pridania >= timezone.now() - datetime.timedelta(days=1)
+        return self.datum_aktualizacie >= timezone.now() - datetime.timedelta(days=1)
 
 #1057	TrnkaAlfred	atrnka@truni.sk	prof. RNDr.	Alfréd	Trnka	PhD.	1729
 #   oo = Osoba(rs_uid = 1057, rs_login = "TrnkaAlfred", email = "atrnka@truni.sk", titul_pred_menom = "RNDr.", meno = "Alfréd", priezvisko = "Trnka", titul_za_menom = "PhD.", posobisko = "Truni, Trenčín")
@@ -33,9 +34,10 @@ class Zmluva(models.Model):
     zmluvna_strana = models.ForeignKey(Osoba, on_delete=models.CASCADE)
     cislo_zmluvy = models.CharField("Číslo zmluvy", max_length=50)
     datum_pridania = models.DateTimeField('Dátum pridania')
+    datum_aktualizacie = models.DateTimeField('Dátum aktualizácie')
 
     def __str__(self):
         return self.cislo_zmluvy
-    def nedavno_pridane(self):
-        return self.datum_pridania >= timezone.now() - datetime.timedelta(days=1)
+    def nedavno_aktualizovane(self):
+        return self.datum_aktualizacie >= timezone.now() - datetime.timedelta(days=1)
 
