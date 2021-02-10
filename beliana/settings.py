3d8fefd425eaa96d1fd7cb581212e835b7cb2962
milossramek <msramek22@gmail.com>
2021-02-10 16:14:47 +0100

    Added model classes Osoba and Zmluva

--- beliana/settings.py
+++ beliana/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'zmluvy.apps.ZmluvyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- zmluvy/models.py
+++ zmluvy/models.py
@@ -1,3 +1,41 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Osoba(models.Model):
+    rs_uid = models.IntegerField("Uid v RS")
+    rs_login = models.CharField("Login v RS", max_length=100)
+    email = models.CharField("Email", max_length=200)
+    titul_pred_menom = models.CharField("Titul pred menom", max_length=100)
+    meno = models.CharField("Meno", max_length=200)
+    priezvisko = models.CharField("Priezvisko", max_length=200)
+    titul_za_menom = models.CharField("Titul za menom", max_length=100)
+    posobisko = models.CharField("Pôsobisko", max_length=200)
+    datum_pridania = models.DateTimeField('Dátum pridania')
+    #pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.rs_login
+    def nedavno_pridane(self):
+        return self.datum_pridania >= timezone.now() - datetime.timedelta(days=1)
+
+#1057	TrnkaAlfred	atrnka@truni.sk	prof. RNDr.	Alfréd	Trnka	PhD.	1729
+#   oo = Osoba(rs_uid = 1057, rs_login = "TrnkaAlfred", email = "atrnka@truni.sk", titul_pred_menom = "RNDr.", meno = "Alfréd", priezvisko = "Trnka", titul_za_menom = "PhD.", posobisko = "Truni, Trenčín")
+
+#1062	AstalosBoris	boris.astalos@SNM.sk		Boris	Astaloš		40/2019
+#   oo = Osoba(rs_uid = 1062, rs_login = "AstalosBoris", email = "boris.astalos@SNM.sk", meno = "Boris", priezvisko = "Astaloš", posobisko = "Slovenské národné múzeum, Bratislava")
+#z=Zmluva(zmluvna_strana=oo, cislo_zmluvy="40/2019")
+
+    
+
+class Zmluva(models.Model):
+    zmluvna_strana = models.ForeignKey(Osoba, on_delete=models.CASCADE)
+    cislo_zmluvy = models.CharField("Číslo zmluvy", max_length=50)
+    datum_pridania = models.DateTimeField('Dátum pridania')
+
+    def __str__(self):
+        return self.cislo_zmluvy
+    def nedavno_pridane(self):
+        return self.datum_pridania >= timezone.now() - datetime.timedelta(days=1)
+
