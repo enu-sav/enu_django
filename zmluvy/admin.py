f4fb1dab9dbf11d152b66c0297a81def6210ffd6
milossramek <msramek22@gmail.com>
2021-03-22 14:10:29 +0100

    search in admin views extended to several fields

--- zmluvy/admin.py
+++ zmluvy/admin.py
@@ -8,7 +8,7 @@ class OsobaAutorAdmin(admin.ModelAdmin):
     list_display = ('rs_login', 'rs_uid', 'email', 'titul_pred_menom', 'meno', 'priezvisko', 'titul_za_menom', 'rodne_cislo', 'odbor', "adresa_ulica", "adresa_mesto", "adresa_stat", 'datum_aktualizacie')
     ordering = ('datum_aktualizacie',)
     #search_fields = ('rs_login', 'priezvisko')
-    search_fields = ['rs_login']
+    search_fields = ['rs_login', 'r_uid', 'email']
 
     #obj is None during the object creation, but set to the object being edited during an edit
     def get_readonly_fields(self, request, obj=None):
@@ -20,9 +20,10 @@ class OsobaAutorAdmin(admin.ModelAdmin):
 admin.site.register(OsobaAutor, OsobaAutorAdmin)
 
 class ZmluvaAutorAdmin(admin.ModelAdmin):
-    list_display = ('cislo_zmluvy', 'zmluvna_strana', 'datum_pridania', 'datum_aktualizacie')
+    list_display = ('cislo_zmluvy', 'zmluvna_strana', 'odmena', 'datum_pridania', 'datum_aktualizacie')
     ordering = ('datum_aktualizacie',)
-    search_fields = ['cislo_zmluvy']
+    #search_fields = ['cislo_zmluvy']
+    search_fields = ['cislo_zmluvy','zmluvna_strana__rs_login', 'odmena']
 
     #obj is None during the object creation, but set to the object being edited during an edit
     def get_readonly_fields(self, request, obj=None):
