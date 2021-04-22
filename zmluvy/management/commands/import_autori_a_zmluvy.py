baa6666f7b96bbbd4af936f6d9f3e0bb18470e18
milossramek <msramek22@gmail.com>
2021-04-22 10:12:52 +0200

    import_autori_a_zmluvy: enable several contracts for an author

--- zmluvy/management/commands/import_autori_a_zmluvy.py
+++ zmluvy/management/commands/import_autori_a_zmluvy.py
@@ -41,25 +41,34 @@ class Command(BaseCommand):
         #"#","Súbor","číslo zmluvy","Titul pred","Meno","Priezvisko","Titul za","Adresa1","Adresa2","Adresa3","Rodné číslo","IBAN","e-mail","Odbor","Dohodnutá odmena","Dátum CRZ","Url zmluvy","Zdaniť","Zomrel"
 
         for autor in data:
-            #trace()
+            if autor[hdr["Priezvisko"]] == "Skovajsa":
+                trace()
+                pass
             login = self.transliterate(autor[hdr["Priezvisko"]])+self.transliterate(autor[hdr["Meno"]])
-            if not valid_iban(autor[hdr["IBAN"]]):
+            if autor[hdr["IBAN"]] and not valid_iban(autor[hdr["IBAN"]]):
                 self.stdout.write(self.style.ERROR(f"chybný IBAN: {login} {autor[hdr['číslo zmluvy']]} {autor[hdr['IBAN']]}"))
-                continue
             o_query_set = OsobaAutor.objects.filter(rs_login=login)
             if o_query_set:
                 oo = o_query_set[0]
                 if autor[hdr["Titul pred"]]: oo.titul_pred_menom = autor[hdr["Titul pred"]]
                 oo.meno = autor[hdr["Meno"]]
                 oo.priezvisko = autor[hdr["Priezvisko"]]
-                if autor[hdr["Titul za"]]: oo.titul_za_menom = autor[hdr["Titul za"]]
-                if autor[hdr["Adresa1"]]: oo.adresa_ulica = autor[hdr["Adresa1"]]
-                if autor[hdr["Adresa2"]]: oo.adresa_mesto = autor[hdr["Adresa2"]]
-                if autor[hdr["Adresa3"]]: oo.adresa_stat = autor[hdr["Adresa3"]]
-                if autor[hdr["Rodné číslo"]]: oo.rodne_cislo = autor[hdr["Rodné číslo"]]
-                if autor[hdr["IBAN"]]: oo.bankovy_kontakt = autor[hdr["IBAN"]]
-                if autor[hdr["e-mail"]]: oo.email = autor[hdr["e-mail"]]
-                if autor[hdr["Odbor"]]: oo.odbor = autor[hdr["Odbor"]]
+                if autor[hdr["Titul za"]]: 
+                    oo.titul_za_menom = autor[hdr["Titul za"]]
+                if autor[hdr["Adresa1"]]: 
+                    oo.adresa_ulica = autor[hdr["Adresa1"]]
+                if autor[hdr["Adresa2"]]: 
+                    oo.adresa_mesto = autor[hdr["Adresa2"]]
+                if autor[hdr["Adresa3"]]: 
+                    oo.adresa_stat = autor[hdr["Adresa3"]]
+                if autor[hdr["Rodné číslo"]]: 
+                    oo.rodne_cislo = autor[hdr["Rodné číslo"]]
+                if autor[hdr["IBAN"]]: 
+                    oo.bankovy_kontakt = autor[hdr["IBAN"]]
+                if autor[hdr["e-mail"]]: 
+                    oo.email = autor[hdr["e-mail"]]
+                if autor[hdr["Odbor"]]: 
+                    oo.odbor = autor[hdr["Odbor"]]
                 if autor[hdr["Zomrel"]]:
                     oo.poznamka = "Autor zomrel"
                 if autor[hdr["Zdaniť"]] == "nie":
@@ -69,23 +78,36 @@ class Command(BaseCommand):
                 if autor[hdr["Url zmluvy"]]:
                     oo.url_zmluvy = autor[hdr["Url zmluvy"]]
                 oo.save()
-                self.stdout.write(self.style.SUCCESS(f"OK: {login}"))
+
+                # pridať zmluvu
                 if autor[hdr["číslo zmluvy"]]:
                     o_query_set = ZmluvaAutor.objects.filter(zmluvna_strana=oo)
-                    if o_query_set:
-                        zm = o_query_set.first()
-                    else:
-                        zm = ZmluvaAutor.objects.create(zmluvna_strana=oo)
+                    self.stdout.write(self.style.SUCCESS(f"OK: {login}, zmluva {autor[hdr['číslo zmluvy']]}"))
+                    # zistiť, či už zmluvu máme
+                    zm = None
+                    for zz in o_query_set:
+                        if zz.cislo_zmluvy == autor[hdr["číslo zmluvy"]]:
+                            zm = zz
+                            break
+                    if zm:
+                        continue
+
+                    zm = ZmluvaAutor.objects.create(zmluvna_strana=oo)
                     zm.odmena = autor[hdr["Dohodnutá odmena"]]
                     zm.cislo_zmluvy = autor[hdr["číslo zmluvy"]]
 
                     if autor[hdr["Dátum CRZ"]] and autor[hdr["Url zmluvy"]]:
                         #zm.datum_zverejnenia_CRZ = autor[hdr["Dátum CRZ"]]
-                        zm.datum_zverejnenia_CRZ = datetime.strptime(autor[hdr["Dátum CRZ"]],"%d.%m.%Y")
+                        if "-" in autor[hdr["Dátum CRZ"]]:
+                            zm.datum_zverejnenia_CRZ = datetime.strptime(autor[hdr["Dátum CRZ"]],"%Y-%m-%d")
+                        else:
+                            zm.datum_zverejnenia_CRZ = datetime.strptime(autor[hdr["Dátum CRZ"]],"%d.%m.%Y")
                         zm.url_zmluvy = autor[hdr["Url zmluvy"]]
                         zm.stav_zmluvy = StavZmluvy.ZVEREJNENA_V_CRZ
                     datum_pridania = timezone.now()
                     zm.datum_aktualizacie = timezone.now()
                     zm.save()
+                else:
+                    self.stdout.write(self.style.WARNING(f"OK: {login}"))
             else:
                 self.stdout.write(self.style.ERROR(f"Nenájdené medzi autormi: {login}"))
