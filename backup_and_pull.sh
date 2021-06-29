99a82c19747adda7631b3405d1ff4eae3c43b52c
milossramek <msramek22@gmail.com>
2021-06-29 10:02:40 +0200

    backup_an_push.sh: parameterize current directory name

--- backup_and_pull.sh
+++ backup_and_pull.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 sudo service enu_django stop
 echo `git log --pretty=format:'%h' -n 1` > commit
+curdir=${PWD##*/}
 (
 cd ..
-tar cvzf enu_django-`date --iso-8601`.tgz --exclude=.env enu_django
+tar cvzf $curdir-`date --iso-8601`.tgz --exclude=.env $curdir
 )
 git pull
 ./manage.py makemigrations
