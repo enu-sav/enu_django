29c40352b4d3ee6ebc9471972dc7ecafd12e4cd1
milossramek <msramek22@gmail.com>
2021-06-29 09:41:52 +0200

    modified the pull.sh script - make backup

--- pull.sh
+++ pull.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+sudo service enu_django stop
+echo `git log --pretty=format:'%h' -n 1` > commit
+(
+cd ..
+tar cvzf enu_django-`date --iso-8601`.tgz enu_django
+)
 git pull
 ./manage.py makemigrations
 ./manage.py migrate
+sudo service enu_django start
