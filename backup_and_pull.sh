1c80478603758e3c2adea4ad68d83f1e9cd9bd62
milossramek <msramek22@gmail.com>
2021-06-30 13:37:06 +0200

    backup_and_pull.sh changed

--- backup_and_pull.sh
+++ backup_and_pull.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
-sudo service enu_django stop
-echo `git log --pretty=format:'%h' -n 1` > commit
+# aktualny priecinok musi byt pomenovany ako prislusna sluzba
+# enu_django
+# enu_django-dev
+
 curdir=${PWD##*/}
+sudo service $curdir stop
+echo `git log --pretty=format:'%h' -n 1` > commit
+
+dt=`date -I`
+echo Backing up database content to dump-$dt.json
+./manage.py dumpdata --natural-foreign --natural-primary -e contenttypes -e auth.Permission --indent 2 > dump-$dt.json
+
 (
+echo Archiving everything to `pwd`/$dt
 cd ..
-tar cvzf $curdir-`date --iso-8601`.tgz --exclude=.env $curdir
+tar czf $curdir-$dt.tgz --exclude=.env $curdir
 )
 git pull
-./manage.py makemigrations
+./manage.py makemigrations zmluvy
 ./manage.py migrate
-sudo service enu_django start
+./manage.py loaddata dump-$dt.json
+sudo service $curdir start
