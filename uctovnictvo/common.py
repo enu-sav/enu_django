08b1de30b360cdff1c7ec91fc4854c340a3539a3
milossramek <msramek22@gmail.com>
2021-10-10 19:49:44 +0200

    Dokončené generovanie dohody v VytvoritSuborDohody

--- uctovnictvo/common.py
+++ uctovnictvo/common.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.contrib import messages
 from django.utils import timezone
 from .models import SystemovySubor, PrijataFaktura, AnoNie, Objednavka, PrijataFaktura, Rozhodnutie, Zmluva
-from .models import DoVP, DoPC
+from .models import DoVP, DoPC, Poistovna, TypDochodku
 
 from openpyxl import load_workbook
 from openpyxl.styles import Font, Color, colors, Alignment, PatternFill , numbers
@@ -15,6 +15,21 @@ from openpyxl.utils import get_column_letter
 def locale_format(d):
     return locale.format('%%0.%df' % (-d.as_tuple().exponent), d, grouping=True)
 
+def meno_priezvisko(autor):
+    if autor.titul_pred_menom:
+        mp = f"{autor.titul_pred_menom} {autor.meno} {autor.priezvisko}"
+    else:
+        mp = f"{autor.meno} {autor.priezvisko}"
+    if autor.titul_za_menom:
+        mp = f"{mp}, {autor.titul_za_menom}"
+    return mp.strip()
+
+def adresa(osoba):
+    if osoba.adresa_ulica:
+        return f"{osoba.adresa_ulica}, {osoba.adresa_mesto}, {osoba.adresa_stat}".strip()
+    else:
+        return f"{osoba.adresa_mesto}, {osoba.adresa_stat}".strip()
+            
 def VytvoritPlatobnyPrikaz(faktura):
     #úvodné testy
     if not os.path.isdir(settings.PLATOBNE_PRIKAZY_DIR):
@@ -91,9 +106,14 @@ def VytvoritPlatobnyPrikaz(faktura):
 
 
 # skryt sekciu v dokumente dohody
-# sname: sekcia, ktorá sa má zachovať 
-def dohoda_skryt_sekcie(text, sname):
+#sekcia: napr. 'text:name="DoBPS_podmienky"'. Zoznam je vo funkcii dohoda_skryt_sekcie
+def dohoda_skryt_sekciu(text, sekcia):
+    return text.replace(sekcia, f'{sekcia} text:display="none"')
+
+# dtype: sekcia podľa typu zmluvy, ktorá sa má zachovať 
+def dohoda_skryt_sekcie(text, dtype):
     #Sekcie v dokumente, ktoré možno skryť 
+    #Po pridaní novej sekcie treba sem pridať
     sekcie = [
         'text:name="DoBPS_podmienky"',
         'text:name="DoVP_vyplata"',
@@ -101,23 +121,50 @@ def dohoda_skryt_sekcie(text, sname):
         'text:name="DoBPS_vyplata"',
         'text:name="DoPC_DoBPS_paska"',
         'text:name="DoPC_DoVP_vyhlasenie"',
-        'text:name="DoBPS_vyhlasenie"'
+        'text:name="DoBPS_vyhlasenie"',
+        'text:name="DoBPS_priloha"',
+        'text:name="DoVP_pomocnik"'
     ]
     for sekcia in sekcie:
-       if not sname in sekcia:
+       if not dtype in sekcia:
         text = text.replace(sekcia, f'{sekcia} text:display="none"')
     return text
 
+def OveritUdajeDohodara(dohodar):
+    chyby = ""
+    if not dohodar.meno: chyby = f"{chyby} meno,"
+    if not dohodar.priezvisko: chyby = f"{chyby} priezvisko,"
+    if not dohodar.rodne_cislo: chyby = f"{chyby} rodné číslo,"
+    if not dohodar.bankovy_kontakt: chyby = f"{chyby} bankový kontakt,"
+    if not dohodar.adresa_mesto: chyby = f"{chyby} PSČ a mesto,"
+    # ulica sa netestuje, môže byť nezadaná
+    #if not dohodar.adresa_ulica: chyby = f"{chyby} ulica,"
+    if not dohodar.adresa_stat: chyby = f"{chyby} štát,"
+    if not dohodar.miesto_nar: chyby = f"{chyby} miesto narodenia," 
+    if not dohodar.cop: chyby = f"{chyby} číslo obč. preukazu,"
+    if not dohodar.datum_nar: chyby = f"{chyby} dátum narodenia," 
+    if not dohodar.poistovna: chyby = f"{chyby} zdravotná poisťovňa," 
+    if not dohodar.email: chyby = f"{chyby} email,"
+    if not dohodar.stav: chyby = f"{chyby} rodinný stav,"
+    if not dohodar.st_prislusnost: chyby = f"{chyby} štátna príslušnosť,"
+    if not dohodar.poberatel_doch: chyby = f"{chyby} poberateľ dôchodku," 
+    if dohodar.poberatel_doch == AnoNie.ANO:
+        if not dohodar.typ_doch: chyby = f"{chyby} typ dôchodku," 
+        if not dohodar.datum_doch: chyby = f"{chyby} dátum vzniku dôchodku," 
+    return chyby.strip(" ").strip(",")
+
+
+# Vytvorí dohodu, krycí list a všetko, čo k tomu treba
 def VytvoritSuborDohody(dohoda):
+    chyby = OveritUdajeDohodara(dohoda.zmluvna_strana)
+    if chyby:
+        return messages.ERROR, f"Dohoda nebola vytvorená, lebo údaje dohodára sú nekomplentné. Chýba: {chyby}", None
+
     #úvodné testy
     dohody_dir_path  = os.path.join(settings.MEDIA_ROOT,settings.DOHODY_DIR)
     if not os.path.isdir(dohody_dir_path):
         os.makedirs(dohody_dir_path)
     
-    # nacitat sablonu
-    lt="[["
-    gt="]]"
-
     #Načítať súbor šablóny
     nazov_objektu = "Šablóna dohody"  #Presne takto musí byť objekt pomenovaný
     sablona = SystemovySubor.objects.filter(subor_nazov = nazov_objektu)
@@ -132,49 +179,71 @@ def VytvoritSuborDohody(dohoda):
         return messages.ERROR, f"Chyba pri vytváraní súboru platobného príkazu faktúry: chyba pri čítaní šablóny '{nazov_suboru}'", None
     
     #Create directory admin.rs_login if necessary
-    trace()
     # vložiť spoločné údaje
-    text.replace("[[cislo]]", dohoda.cislo)
-    text.replace("[[meno_priezvisko]]", "asdf")
-    text.replace("[[rod_priezvisko]]", "asdf")
-    text.replace("[[adresa]]", "asdf")
-    text.replace("[[d_narodenia]]", "asdf")
-    text.replace("[[m_narodenia]]", "asdf")
-    text.replace("[[cop]]", "asdf")
-    text.replace("[[rc]]", "asdf")
-    text.replace("[[stav]]", "asdf")
-    text.replace("[[st_prislusnost]]", "asdf")
-    text.replace("[[zdrav_poistovna]]", "asdf")
-    text.replace("[[dochodok]]", "asdf")
-    text.replace("[[dohodnuta_cinnost]]", dohoda.predmet)
-    text.replace("[[datum od]]", dohoda.datum_od.strftime("%d. %m. %Y"))
-    text.replace("[[datum_do]]", dohoda.datum_do.strftime("%d. %m. %Y"))
-    text.replace("[[rozsah_prace_cas]]", "asdf")
-    text.replace("[[preberajuca_osoba]]", "asdf")
-    text.replace("[[osobne_pomoc]]", "asdf")
-    text.replace("[[pomocnik]]", "asdf")
-    text.replace("[[IBAN]]", "asdf")
-    text.replace("[[vyplatny_termin]]", "asdf")
-    text.replace("[[email]]", "asdf")
+    dohodar = dohoda.zmluvna_strana
+    text = text.replace("[[meno_priezvisko]]", meno_priezvisko(dohodar))
+    if dohodar.rod_priezvisko:
+        text = text.replace("[[rod_priezvisko]]", f", rod. priezvisko: {dohodar.rod_priezvisko}")
+    else:
+        text = text.replace("[[rod_priezvisko]]", "")
+    text = text.replace("[[adresa]]", adresa(dohodar))
+    text = text.replace("[[d_narodenia]]", dohodar.datum_nar.strftime("%d. %m. %Y"))
+    text = text.replace("[[m_narodenia]]", dohodar.miesto_nar)
+    text = text.replace("[[cop]]", dohodar.cop)
+    text = text.replace("[[rc]]", dohodar.rodne_cislo)
+    text = text.replace("[[stav]]", dohodar.stav)
+    text = text.replace("[[st_prislusnost]]", dohodar.st_prislusnost)
+    text = text.replace("[[zdrav_poistovna]]", Poistovna(dohodar.poistovna).label)
+    text = text.replace("[[IBAN]]", dohodar.bankovy_kontakt)
+    text = text.replace("[[email]]", dohodar.email)
+    if dohodar.poberatel_doch == AnoNie.ANO:
+        text = text.replace( "[[dochodok]]", 
+                f"{AnoNie(dohodar.poberatel_doch).label}, {TypDochodku(dohodar.typ_doch).label}, dátum vzniku: {dohodar.datum_doch.strftime('%d. %m. %Y')}")
+    else:
+        text = text.replace("[[dochodok]]", AnoNie(dohodar.poberatel_doch).label)
 
-    #trace()
+    text = text.replace("[[dohodnuta_cinnost]]", dohoda.predmet)
+    text = text.replace("[[cislo]]", dohoda.cislo)
+    text = text.replace("[[datum_od]]", dohoda.datum_od.strftime("%d. %m. %Y"))
+    text = text.replace("[[datum_do]]", dohoda.datum_do.strftime("%d. %m. %Y"))
+    text = text.replace("[[datum]]", timezone.now().strftime("%d. %m. %Y"))
+    text = text.replace("[[zdroj]]", dohoda.zdroj.kod)
+    text = text.replace("[[zakazka]]", dohoda.zakazka.kod)
+
     # vložiť údaje DoVP
     if type(dohoda) == DoVP:
-        text.replace("[[typ_dohody]]", "o vykonaní práce")
-        text.replace("[[zakony]]", "asdf")
-        text.replace("[[odmena]]", "asdf")
+        text = text.replace("[[typ_dohody]]", "o vykonaní práce")
+        text = text.replace("[[zakony]]", "§ 223 – § 225 a § 226")
+        text = text.replace("[[odmena]]", f"{dohoda.odmena_celkom} Eur")
+        text = text.replace("[[rozsah_prace_cas]]", f"{dohoda.hod_celkom} hodín")
+        if dohoda.pomocnik:
+            text = text.replace("[[osobne_pomoc]]", "za pomoci rod. príslušníkov ")
+            text = text.replace("[[pomocnik]]", dohoda.pomocnik)
+        else:
+            text = text.replace("[[osobne_pomoc]]", "osobne")
+            text = text.replace("[[pomocnik]]", "")
+        text = text.replace("[[vyplatny_termin]]", dohoda.datum_do.strftime("%m/%Y")) 
         text = dohoda_skryt_sekcie(text, "DoVP")
+        if dohodar.poberatel_doch == AnoNie.NIE:
+            text = dohoda_skryt_sekciu(text, 'text:name="DoPC_DoVP_vyhlasenie"')
     # vložiť údaje DoPC
     elif type(dohoda) == DoPC:
-        text.replace("[[typ_dohody]]", "o pracovnej činnosti")
-        text.replace("[[zakony]]", "§223 - §225 a §228a")
-        text.replace("[[odmena]]", "asdf")
+        text = text.replace("[[typ_dohody]]", "o pracovnej činnosti")
+        text = text.replace("[[zakony]]", "§ 223 – § 225 a § 228a")
+        text = text.replace("[[odmena]]", f"{dohoda.odmena_mesacne} Eur mesačne")
+        text = text.replace("[[rozsah_prace_cas]]", f"{dohoda.hod_mesacne} hodín mesačne")
         text = dohoda_skryt_sekcie(text, "DoPC")
+        if dohodar.poberatel_doch == AnoNie.NIE:
+            text = dohoda_skryt_sekciu(text, 'text:name="DoPC_DoVP_vyhlasenie"')
     # vložiť údaje DoBPS
-    #elif type(dohoda) == DoBPS:
-        #text.replace("[[typ_dohody]]", "o brigádnickej práci študentov")
-        #text.replace("[[odmena]]", "asdf")
-        #text.replace("[[zakony]]", "asdf")
+    elif type(dohoda) == DoBPS:
+        text = text.replace("[[typ_dohody]]", "o brigádnickej práci študentov")
+        text = text.replace("[[zakony]]", "§ 223 – § 225, § 227 a § 228")
+        text = text.replace("[[odmena]]", f"{dohoda.odmena_mesacne} Eur mesačne")
+        text = text.replace("[[rozsah_prace_cas]]", "asdf")
+        text = dohoda_skryt_sekcie(text, "DoBPS")
+        #text.replace("[[preberajuca_osoba]]", "asdf")
+
     nazov = f"{dohoda.cislo}-{dohoda.zmluvna_strana.priezvisko}.fodt"
     opath = os.path.join(settings.DOHODY_DIR,nazov)
     with open(os.path.join(settings.MEDIA_ROOT,opath), "w") as f:
