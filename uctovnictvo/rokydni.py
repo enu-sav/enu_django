6abd4a91bab18e0ab3952b5b67480645a8ba80d3
milossramek <msramek22@gmail.com>
2021-11-10 10:52:10 +0100

    uctovnictvo/rokydni.py, added comment

--- uctovnictvo/rokydni.py
+++ uctovnictvo/rokydni.py
@@ -81,6 +81,7 @@ def vypocet_vek(datumnarodenia, datum):
 
 def datum_postupu(datum_nastupu, datum, zapocitana_prax):
     # Podľa: Zákon č. 553/2003 Z. z.
+    # Vysvetlenie: do 32 rokov praxe platí stupeň 12, teda pri dosiahnutí 32 praxe sa zvyšuje na 13
     roky_postupu = [0, 2, 4, 6, 9, 12, 15, 18, 21, 24, 28, 32, 36, 40]
     # nový stupeň po dosiahnutí rokov postupu
     plat_stupen = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"]
