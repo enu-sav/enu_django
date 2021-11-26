ad99ed73a2ac9a3a6e6596e0d670e43a6948909c
milossramek <msramek22@gmail.com>
2021-11-26 18:46:10 +0100

    Added tests to uctovnictvo/odvody.py

--- uctovnictvo/odvody.py
+++ uctovnictvo/odvody.py
@@ -66,3 +66,38 @@ def DohodarOdvodySpolu(meno_suboru, odmena, typ, vodmena):
         odvody_zam = sum([odmena * tz[tt] for tt in tz])
         odvody_prac = sum([odmena * tp[tt] for tt in tp])
     return round(odvody_zam, 2), round(odvody_prac, 2) 
+
+def main():
+    smeno = '/home/milos/Beliana/Django/enu_django-dev/data/Subory/SablonyASubory/OdvodyZamestnanciDohodari.xlsx'
+    suma = 100
+    vodmena = 200
+
+    print("DoPC 100 (34.95, 13.4) ", DohodarOdvodySpolu(smeno, suma, "DoPC", 0))
+    print("DoVP 100 (32.55, 11.0)", DohodarOdvodySpolu(smeno, suma, "DoVP", 0))
+    print("DoBPS 100 (22.55, 7.0)", DohodarOdvodySpolu(smeno, suma, "DoBPS", 0))
+    print("InvDoch 100 (22.55, 7.0)", DohodarOdvodySpolu(smeno, suma, "StarDoch", 0))
+    print("StarDoch 100 (19.55, 4.0)", DohodarOdvodySpolu(smeno, suma, "InvDoch", 0))
+    print()
+
+    print("DoPC 100 (34.95, 13.4)", DohodarOdvodySpolu(smeno, suma, "DoPC", 200))
+    print("DoVP 100 (32.55, 11.0)", DohodarOdvodySpolu(smeno, suma, "DoVP", 200))
+    print("DoBPS 100 (1.6, 0.0)", DohodarOdvodySpolu(smeno, suma, "DoBPS", 200))
+    print("StarDoch 100 (1.6, 0.0)", DohodarOdvodySpolu(smeno, suma, "StarDoch", 200))
+    print("InvDoch 100 (1.6, 0.0)", DohodarOdvodySpolu(smeno, suma, "InvDoch", 200))
+    print()
+
+    print("DoPC 200 (69.9, 26.8)", DohodarOdvodySpolu(smeno, 2*suma, "DoPC", 200))
+    print("DoVP 200 (65.1, 22.0)", DohodarOdvodySpolu(smeno, 2*suma, "DoVP", 200))
+    print("DoBPS 200 (1.6, 0.0)", DohodarOdvodySpolu(smeno, 2*suma, "DoBPS", 200))
+    print("StarDoch 200 (1.6, 0.0)", DohodarOdvodySpolu(smeno, 2*suma, "StarDoch", 200))
+    print("InvDoch 200 (1.6, 0.0)", DohodarOdvodySpolu(smeno, 2*suma, "InvDoch", 200))
+    print()
+
+    print("DoPC 300 (104.85, 40.2)", DohodarOdvodySpolu(smeno, 3*suma, "DoPC", 200))
+    print("DoVP 300 (97.65, 33.0)", DohodarOdvodySpolu(smeno, 3*suma, "DoVP", 200))
+    print("DoBPS 300 (24.15, 7.0)", DohodarOdvodySpolu(smeno, 3*suma, "DoBPS", 200))
+    print("StarDoch 300 (21.15, 4.0)", DohodarOdvodySpolu(smeno, 3*suma, "StarDoch", 200))
+    print("InvDoch 300 (24.15, 7.0)", DohodarOdvodySpolu(smeno, 3*suma, "InvDoch", 200))
+    
+if __name__ == "__main__":
+    main()
