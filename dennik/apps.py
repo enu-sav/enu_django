07a06c0c83a8451171eb09b278b909cb2c40d0fd
milossramek <msramek22@gmail.com>
2022-02-06 19:08:08 +0100

    App Denniky renamed to Spolocne

--- dennik/apps.py
+++ dennik/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class DennikConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dennik'
-    verbose_name = "Denníky"
+    verbose_name = "Spoločné"
