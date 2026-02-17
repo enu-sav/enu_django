0f68434964f4425744093370d9896d335185055f
milossramek <msramek22@gmail.com>
2026-02-17 10:29:53 +0100

    Activated quick lookup (q=) for some content types

--- beliana/urls.py
+++ beliana/urls.py
@@ -35,10 +35,35 @@ from django.conf.urls.static import static # new
 
 from ipdb import set_trace as trace
 
+#Umožniť priamy prístup k niektorým položkám prostredníctvom /q=PP-2026-001 a /q=VO-2-2026-001 a podobne
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.http import Http404
+import re
+from zmluvy.models import PravoNaPouzitie, VytvarnaObjednavkaPlatba
+def quick_lookup(request, cislo):
+    if re.findall(f"{PravoNaPouzitie.oznacenie}-....-...", cislo):
+        obj = PravoNaPouzitie.objects.filter(cislo=cislo).first()
+        if not obj:
+            raise Http404(f"Nesprávny identifikátor objektu. Povolené hodnoty sú '{PravoNaPouzitie.oznacenie}-RRRR-NNN")
+        # This reverses to the admin change page
+        url = reverse('admin:zmluvy_pravonapouzitie_change', args=[obj.pk])
+        return redirect(url)
+    elif re.findall(f"{VytvarnaObjednavkaPlatba.oznacenie}-....-...", cislo):
+        obj = VytvarnaObjednavkaPlatba.objects.filter(cislo=cislo).first()
+        if not obj:
+            raise Http404(f"Nesprávny identifikátor objektu. Povolené hodnoty sú '{PravoNaPouzitie.oznacenie}-RRRR-NNN")
+        # This reverses to the admin change page
+        url = reverse('admin:zmluvy_vytvarnaobjednavkaplatba_change', args=[obj.pk])
+        return redirect(url)
+    else:
+        raise Http404(f"Nesprávny identifikátor objektu. Povolené hodnoty sú '{PravoNaPouzitie.oznacenie}-RRRR-NNN, {VytvarnaObjednavkaPlatba.oznacenie}-RRRR-NNN")
+
 #trace()
 urlpatterns = [
     path('', RedirectView.as_view(url=reverse_lazy('admin:index'))),
     path('admin/', admin.site.urls),
+    path('q=<str:cislo>', quick_lookup),
     path('export_action/', include("admin_export_action.urls", namespace="admin_export_action")),
     # Recent versions of Python however have special syntax to include an iterable in a list by using the asterisk (*)
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # needed because of file upload
