3f11dee9d908f4e11a2a278134ade6603a33d444
milossramek <msramek22@gmail.com>
2026-02-19 19:40:46 +0100

    Fixed help text in Objednavka.objednane_polozky

--- uctovnictvo/objednavka_actions.py
+++ uctovnictvo/objednavka_actions.py
@@ -149,8 +149,8 @@ def VytvoritSuborObjednavky(objednavka, username):
             pole = "pole" if pocet_poli==1 else "polia" if pocet_poli < 5 else "polí"
             return f"{co}: Prvá položka má {pocet_poli} {pole}, povolený počet je 2 alebo 5 (skontrolujte oddeľovače). {sn}"
        
-        #if pocet_poli == 2 and pocet_poloziek > 1:
-            #return f"{co}: Ak má prvý riadok len dve polia, tak v Objednané položky môže byť len jeden riadok. {sn}"
+        if pocet_poli == 2 and pocet_poloziek > 1:
+            return f"{co}: Ak má prvý riadok len dve polia, tak v Objednané položky môže byť len jeden riadok. {sn}"
         for rr, polozka in enumerate(polozky):
             plen = len(polozka.split(oddelovac))
             if plen != pocet_poli:
